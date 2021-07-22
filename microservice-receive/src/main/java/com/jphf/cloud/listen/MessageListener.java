4cc6c21d466c8b989e24e0b469dc099cc26dc06a
jphf <jphf0000@gmail.com>
2021-07-22 20:38:59 +0800

    manual ack

--- microservice-receive/src/main/java/com/jphf/cloud/listen/MessageListener.java
+++ microservice-receive/src/main/java/com/jphf/cloud/listen/MessageListener.java
@@ -3,6 +3,7 @@ package com.jphf.cloud.listen;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
 import com.jphf.cloud.shared.UserMessage;
@@ -10,11 +11,13 @@ import com.jphf.cloud.shared.UserMessage;
 @Component
 public class MessageListener {
 
-	private static final Logger logger = LoggerFactory.getLogger(MessageListener.class); 
-	
-	@KafkaListener(topics ="messages.topic")
-	public void handle(UserMessage userMessage) {
+	private static final Logger logger = LoggerFactory.getLogger(MessageListener.class);
+
+	@KafkaListener(topics = "messages.topic")
+	public void handle(UserMessage userMessage, Acknowledgment acknowledgment) {
 		logger.info("{}", userMessage.getMessage().getText());
+
+		acknowledgment.acknowledge();
 	}
-	
+
 }
